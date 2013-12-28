b498784886f2b94b8a630c69163b4ab7389ae52c
Andrei Helo <andreihelo@gmail.com>
2013-12-28 15:43:57 -0700

    Finalización de ejercicios del capítulo 3, incluyendo la configuración avanzada (Postgresql, Guard y Spork).

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'StaticPages' do
+  let(:base_title) { 'Aplicación de Ejemplo del Tutorial de Ruby on Rails' }
+
   describe 'Home page' do
     it "should have the content 'Aplicación de Ejemplo'" do
       visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe 'StaticPages' do
 
     it "should have the title 'Inicio'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Aplicación de Ejemplo del Tutorial de Ruby on Rails | Inicio')
+      expect(page).to have_title("#{base_title} | Inicio")
     end
   end
 
@@ -21,7 +23,7 @@ describe 'StaticPages' do
 
     it "should have the title 'Ayuda'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Aplicación de Ejemplo del Tutorial de Ruby on Rails | Ayuda')
+      expect(page).to have_title("#{base_title} | Ayuda")
     end
   end
 
@@ -33,7 +35,19 @@ describe 'StaticPages' do
 
     it "should have the title '¿Quiénes somos?'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Aplicación de Ejemplo del Tutorial de Ruby on Rails | ¿Quiénes somos?')
+      expect(page).to have_title("#{base_title} | ¿Quiénes somos?")
+    end
+  end
+
+  describe 'Contact page' do
+    it "should have the content 'Contacto'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contacto')
+    end
+
+    it "should have the title 'Contacto'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contacto")
     end
   end
 end
